d591c62b43ca34b3d199ae25703a78e43247ae5d
Nafiur Rahman Khadem <shafinkhadem@gmail.com>
2025-08-29 01:48:47 -0600

    shard requests by key

--- kvs/client/main.go
+++ kvs/client/main.go
@@ -50,8 +50,12 @@ func (client *Client) Put(key string, value string) {
 	}
 }
 
-func runClient(id int, addr string, done *atomic.Bool, workload *kvs.Workload, resultsCh chan<- uint64) {
-	client := Dial(addr)
+func runClient(id int, hosts []string, done *atomic.Bool, workload *kvs.Workload, resultsCh chan<- uint64) {
+	len_hosts := len(hosts)
+	clients := make([]*Client, len_hosts)
+	for i, addr := range hosts {
+		clients[i] = Dial(addr)
+	}
 
 	value := strings.Repeat("x", 128)
 	const batchSize = 1024
@@ -63,9 +67,9 @@ func runClient(id int, addr string, done *atomic.Bool, workload *kvs.Workload, r
 			op := workload.Next()
 			key := fmt.Sprintf("%d", op.Key)
 			if op.IsRead {
-				client.Get(key)
+				clients[op.Key%uint64(len_hosts)].Get(key)
 			} else {
-				client.Put(key, value)
+				clients[op.Key%uint64(len_hosts)].Put(key, value)
 			}
 			opsCompleted++
 		}
@@ -113,11 +117,10 @@ func main() {
 	done := atomic.Bool{}
 	resultsCh := make(chan uint64)
 
-	host := hosts[0]
 	clientId := 0
 	go func(clientId int) {
 		workload := kvs.NewWorkload(*workload, *theta)
-		runClient(clientId, host, &done, workload, resultsCh)
+		runClient(clientId, hosts, &done, workload, resultsCh)
 	}(clientId)
 
 	time.Sleep(time.Duration(*secs) * time.Second)
